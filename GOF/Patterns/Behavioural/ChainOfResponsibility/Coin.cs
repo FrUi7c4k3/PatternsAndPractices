9258b9418b70418394f13e9fff89e83359cbe596
Trevor Langeveldt <scarapple@yahoo.com>
2012-12-10 16:47:36 +0200

    First commit

---
+++ GOF/Patterns/Behavioural/ChainOfResponsibility/Coin.cs
@@ -0,0 +1,8 @@
+﻿namespace Patterns.Behavioural.ChainOfResponsibility
+{
+	public class Coin
+	{
+		public float Weight { get; set; }
+		public float Diameter { get; set; }
+	}
+}
---
+++ GOF/Patterns/Behavioural/ChainOfResponsibility/Handlers/FivePenceHandler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Patterns.Behavioural.ChainOfResponsibility.Handlers
+{
+	public class FivePenceHandler : CoinHandlerBase
+	{
+		public override void HandleCoin(Coin coin)
+		{
+			if (Math.Abs(coin.Weight - 3.25) < 0.02 && Math.Abs(coin.Diameter - 18) < 0.1)
+				Debug.WriteLine("Captured 5p");
+			else if (_successor != null)
+				_successor.HandleCoin(coin);
+		}
+	}
+}
