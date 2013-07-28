b7447749508f9971a0f4b5ad10947a7814a53c7c
Trevor Langeveldt <trevor.langeveldt@gmail.com>
2013-07-28 23:03:05 +0200

    Beginning observable

---
+++ GOF/Patterns/Behavioural/Observer/Location.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Patterns.Behavioural.Observer
+{
+	public class Location
+	{
+		double _lat, _lon;
+
+		public Location(double latitude, double longitude)
+		{
+			_lat = latitude;
+			_lon = longitude;
+		}
+
+		public double Latitude
+		{
+			get { return _lat; }
+			set { _lat = value; }
+		}
+
+		public double Longitude
+		{
+			get { return _lon; }
+			set { _lon = value; }
+		}
+	}
+}
---
+++ GOF/Patterns/Behavioural/Observer/LocationTracker.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Patterns.Behavioural.Observer
+{
+	public class LocationTracker : IObservable<Location>
+	{
+		private IList<IObserver<Location>> _observers;
+
+		public LocationTracker()
+		{
+			_observers = new List<IObserver<Location>>();
+		}
+
+		public IDisposable Subscribe(IObserver<Location> observer)
+		{
+			if (!_observers.Contains(observer))
+			{
+				_observers.Add(observer);
+			}
+
+			return new Unsubscriber(_observers, observer);
+		}
+
+
+	
+		private class Unsubscriber : IDisposable
+		{
+			private IList<IObserver<Location>> _observers;
+			private IObserver<Location> _observer;
+			
+			public Unsubscriber(IList<IObserver<Location>> observers, IObserver<Location> observer)
+			{
+				_observers = observers;
+				_observer = observer;
+			}
+
+			public void Dispose()
+			{
+				if (_observer != null && _observers.Contains(_observer))
+				{
+					_observers.Remove(_observer);
+				}
+			}
+		}
+	}
+}
